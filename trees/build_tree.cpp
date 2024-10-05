c79d2ec857a02e1fc2090bd5106b39b36c63891b
Puli Revanth Pershad <70310699+Revanth-Pershad@users.noreply.github.com>
2024-10-05 12:15:23 +0530

    Update build_tree.cpp

--- trees/build_tree.cpp
+++ trees/build_tree.cpp
@@ -1,1 +1,3 @@
 // We can build a binary tree if atleast 2 traversals are given and one of them is inOrder Traversal.
+// We write a recursive solution that finds the root from the traversals then breaks the traversals left
+// and right subtree and then recursively solves for the both subtrees
