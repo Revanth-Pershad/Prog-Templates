b18bc1626c569eda6f5cc3096b94ba30665707e8
Puli Revanth Pershad <70310699+Revanth-Pershad@users.noreply.github.com>
2024-10-06 18:17:59 +0530

    Update height_tree.cpp

--- trees/height_tree.cpp
+++ trees/height_tree.cpp
@@ -1,1 +1,9 @@
-
+// Find the maximum of the left and right and then add 1 for the current root node and this is the height of the current subtree
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root)return 0;
+        int lft = maxDepth(root->left), rgt = maxDepth(root->right);
+        return max(lft, rgt)+1;
+    }
+};
