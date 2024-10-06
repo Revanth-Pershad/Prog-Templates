f60efa9e7e0e2d0d9ff7638f5ee14fd67d3a0f86
Puli Revanth Pershad <70310699+Revanth-Pershad@users.noreply.github.com>
2024-10-06 18:09:04 +0530

    Update isBalanced_tree.cpp

--- trees/isBalanced_tree.cpp
+++ trees/isBalanced_tree.cpp
@@ -1,1 +1,17 @@
+// Check if a tree is height balanced
 
+
+// We return height if the tree is balances and return -1 if it is not
+class Solution {
+public:
+    int helper(TreeNode *root){
+        if(!root)return 0;
+        int lft = helper(root->left), rgt = helper(root->right);
+        if(lft == -1 || rgt == -1 || abs(lft - rgt) > 1)return -1;
+        return max(lft, rgt)+1;
+    }
+
+    bool isBalanced(TreeNode* root) {
+        return helper(root) == -1 ? false : true;
+    }
+};
