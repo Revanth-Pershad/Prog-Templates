eec91110fbbb6b849287db7600548f7811f09bac
Puli Revanth Pershad <70310699+Revanth-Pershad@users.noreply.github.com>
2024-10-06 18:15:13 +0530

    Update diameter_tree.cpp

--- trees/diameter_tree.cpp
+++ trees/diameter_tree.cpp
@@ -1,1 +1,16 @@
+// Finding the height of the left and the right and then adding them gives the correct value for the diameter of the tree
+class Solution {
+    int mx = 0;
+public:
+    int height(TreeNode *root){
+        if(!root)return NULL;
+        int lft = height(root->left), rgt = height(root->right);
+        mx = max(lft + rgt + 1, mx);
+        return max(lft, rgt)+1;
+    }
 
+    int diameterOfBinaryTree(TreeNode* root) {
+        int ht = height(root);
+        return mx-1; // Since points between two points
+    }
+};
