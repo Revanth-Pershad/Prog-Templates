0baa437e9e754df071703c74446b68ab1c589d45
Puli Revanth Pershad <70310699+Revanth-Pershad@users.noreply.github.com>
2025-01-24 17:00:56 +0530

    Update Topological_Sort_Kahns.cpp

--- graphs/Topological_Sort_Kahns.cpp
+++ graphs/Topological_Sort_Kahns.cpp
@@ -45,3 +45,35 @@ void solve(){
     kahns(edges, n);
 }
 
+
+// WHEN GRAPH IT SELF IS GIVEN INSTEAD OF EDGES
+void solve(){
+    int n; cin >> n;
+    vector<vector<int>> graph(n);
+    int edges; cin >> edges;
+
+    while(edges--){
+        int a, b; cin >> a >> b;
+        graph[a].push_back(b);
+    }
+
+    vector<int> inOrder(n, 0);
+    queue<int> qu;
+    for(int i = 0; i < n; i++){
+        for(int ngh : graph[i])inOrder[ngh]++;
+    }
+
+    for(int i = 0; i < n; i++)
+        if(inOrder[i] == 0)qu.push(i);
+
+
+    while(!qu.empty()){
+        int node = qu.front(); qu.pop();
+        cout << node;
+
+        for(int ngh : graph[node]){
+            inOrder[ngh]--;
+            if(inOrder[ngh] == 0)qu.push(ngh);
+        }
+    }
+}
