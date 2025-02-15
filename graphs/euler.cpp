2f80fdfac7cfc34b9d5df4d877779f2ced92579d
Puli Revanth Pershad <70310699+Revanth-Pershad@users.noreply.github.com>
2025-02-15 21:27:15 +0530

    Create euler.cpp

---
+++ graphs/euler.cpp
@@ -0,0 +1,7 @@
+// Walk - A random traversal of graph Eg: (A to B to C to A to C) 
+// Trail - A walk in which edges cannot be repeated but Vertices can be
+// Euler Circuit - A trail in which the start and the end is the same vertice and covers all the edges
+
+/*
+  Code to be added
+*/
