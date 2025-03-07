3da8c20ef864c5de480349ebc9662754313d95a6
Puli Revanth Pershad <70310699+Revanth-Pershad@users.noreply.github.com>
2025-03-07 21:23:31 +0530

    Update euler.cpp

--- graphs/euler.cpp
+++ graphs/euler.cpp
@@ -2,6 +2,25 @@
 // Trail - A walk in which edges cannot be repeated but Vertices can be
 // Euler Circuit - A trail in which the start and the end is the same vertice and covers all the edges
 
+// 1. Undirected Graph
+// Eulerian Circuit (Cycle):
+
+// Every vertex has an even degree.
+// Eulerian Path:
+
+// Either all vertices have even degree (which means it is an Eulerian circuit) OR
+// Exactly two vertices have odd degree.
+
+// 2. Directed Graph
+// Eulerian Circuit (Cycle):
+
+// Every vertex has equal in-degree and out-degree.
+// Eulerian Path:
+
+// At most one vertex has (out-degree) - (in-degree) = 1.
+// At most one vertex has (in-degree) - (out-degree) = 1.
+// All other vertices have equal in-degree and out-degree.
+
 /*
   Code to be added
 */
