54e5902197a1830c6e97e665e1310d418df972f9
Puli Revanth Pershad <70310699+Revanth-Pershad@users.noreply.github.com>
2025-03-09 00:59:06 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define rep(i, n) for(int i = 0; i < n; i++)
+#define reps(i, a, n) for(int i = a; i <= n; i++)
+#define revrep(i, n) for(int i = n-1; i >= 0; i--)
+#define db(x) cout << x << "\n"; // Debug
+#define si(x) cin >> x
+#define sii(x, y) cin >> x >> y
+#define sll(x) cin >> x
+#define pi(x) cout << x << " "
+#define pll(x) cout << x
+#define pb push_back
+#define mp make_pair
+#define vec vector
+#define F first
+#define S second
+#define all(x) x.begin(), x.end() // Specify start, end iterator
+#define sortall(x) sort(all(x)) // Sort the array
+#define MOD 1000000007
+#define PI 3.1415926535897932384626
+#define line cout << "\n"
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<char, int> pci;
+typedef pair<int, char> pic;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef vector<pii> vpii;
+typedef priority_queue<int> pq;
+typedef map<int, int> mii;
+typedef map<char, int> mci;
+typedef multiset<int> msi;
+typedef unordered_map<int, int> umii;
+typedef pair<int, int> pii;
+typedef pair<pii, ll> ppiil;
+typedef vector<string> vs;
+typedef queue<pii> qp;
+
+void yes(bool lin = false){ cout << "YES"; if(lin)line; else cout << " "; }
+void no(bool lin = false){cout << "NO"; if(lin)line; else cout << " ";}
+
+template <typename T> void _print(vector<T> &arr){ for(auto x : arr) cout << x << " ";}
+template <typename T> void _scan(vector<T> &arr){ rep(i, arr.size()) cin >> arr[i];}
+
+void solve(){
+    
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    solve();
+        // int T;
+
+        // si(T);
+        // while(T--){
+        //     solve();
+        // }
+}
